62f14e8bde6b374295e8ff96437e3d90ce73e64b
Amin Abbaspour <a.abbaspour@gmail.com>
2022-10-16 16:11:57 +1100

    adding more cors stuff

---
+++ netlify/functions/aca-origin-restricted.js
@@ -0,0 +1,36 @@
+exports.handler = async (event, context) => {
+    console.log(event);
+    const {httpMethod, headers} = event;
+    const {origin} = headers || '';
+
+    if (httpMethod === 'OPTIONS') {
+        if (origin.startsWith('https://www.'))
+            return {
+                statusCode: 204,
+                headers: {
+                    "Access-Control-Allow-Origin": origin,
+                    "Access-Control-Allow-Headers": "*",
+                    "Access-Control-Allow-Max-Age": 0
+                }
+            };
+        else {
+            return {
+                statusCode: 400
+            };
+        }
+    }
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": origin,
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Max-Age": 0,
+            "content-type": 'application/json'
+        },
+        body: JSON.stringify({
+            message: `hello ${origin}`,
+            httpMethod,
+        })
+    }
+}
---
+++ netlify/functions/aca-origin-unrestricted.js
@@ -0,0 +1,30 @@
+exports.handler = async (event, context) => {
+    console.log(event);
+    const {httpMethod, headers} = event;
+    const {origin} = headers || '';
+
+    if (httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 204,
+            headers: {
+                "Access-Control-Allow-Origin": origin,
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Max-Age": 0
+            }
+        };
+    }
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": origin,
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Max-Age": 0,
+            "content-type": 'application/json'
+        },
+        body: JSON.stringify({
+            message: `hello ${origin}`,
+            httpMethod,
+        })
+    }
+}
