730de46e836c02aa9c8582e4bfea2b3f7ea78f02
Amin Abbaspour <a.abbaspour@gmail.com>
2022-11-08 15:48:02 -0800

    dynamic svg height

--- netlify/functions/header-svg.js
+++ netlify/functions/header-svg.js
@@ -8,13 +8,16 @@ exports.handler = async (event, context) => {
     console.log(cookies);
     let text = '';
 
+    let max = 0;
+
     for(const [i, c] of cookies.entries()) {
         console.log(i, c);
         text += `<text x="10" y="${(i+1) * 15}" class="heavy">${c}</text>`;
+        max = i + 1;
     }
 
     console.log(text);
-    const svg = `<svg width="600px" height="50px" xmlns="http://www.w3.org/2000/svg">
+    const svg = `<svg width="200px" height="${(max+1) * 15}" xmlns="http://www.w3.org/2000/svg">
     <style>
         .heavy {
         font: 14px "DM Sans";
